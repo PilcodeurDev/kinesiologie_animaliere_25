ed818fa49429fd1a465a4fbbcb4ef98ab54cd6f0
simon despres <simondprs62790@hotmail.fr>
2023-08-30 19:25:45 +0200

    replace BrowserRouter in index.js

--- kinesio_anim_25_controller/frontend/src/components/App.js
+++ kinesio_anim_25_controller/frontend/src/components/App.js
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import HomePage from "./HomePage"
 import Appointment from "./Appointment"
 
 function App() {
   return (
     <>
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/appointment"element={<Appointment />} />
         </Routes>
-      </BrowserRouter>
     </>
   )
 }
