ec3542afebe9ef2b97fb85038950923385d054c6
simon despres <simondprs62790@hotmail.fr>
2023-09-03 12:10:24 +0200

    i can run server but i have erreur 404

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import home
+# from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from api.views import *
+
 
 urlpatterns = [
-    path('', home),
+
+    path('admin/', admin.site.urls),
+    path('', ReactView.as_view(), name="react-view"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework.views import APIView
+from . models import *
+from rest_framework.response import Response
+from . serializer import *
 
-def home(request):
-    return HttpResponse('Hello, Django!')
+# def home(request):
+#     return HttpResponse('Hello, Django!')
+
+
+class ReactView(APIView):
+
+    serializer_class = ReactSerializer
+
+    def get(self, request):
+        output = [{ "employee": output.employee,
+                    "department": output.departement}
+                    for output in React.objects.all()]
+        return Response(output)
+
+    def post(self, request):
+        serializer = ReactSerializer(data=request.data)
+        if serializer.is_valid(raise_exeption=True):
+            serializer.save()
+            return Response(serializer.data)
