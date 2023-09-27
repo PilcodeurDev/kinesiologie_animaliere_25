d9623acb758adef6df7305f83e022e1f8eb529bd
simon despres <simondprs62790@hotmail.fr>
2023-09-27 18:44:11 +0200

    setup tailwindcss

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,6 +7,13 @@ export default function Home() {
       <div className="banner">
         <h1>Kinésiologie Animalière 25</h1>
       </div>
+      <section className="bulle">
+        <h2 className="bg-blue-400">La kinésiologie animalière</h2>
+        <span>
+          Retrouver l'harmonie intérieure de votre animal grâce à la libération
+          émotionelle en profondeur.
+        </span>
+      </section>
       {/* Footer  */}
     </div>
   );
