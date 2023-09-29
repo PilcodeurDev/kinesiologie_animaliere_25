3047940bde52bc29ea90c141acef62e5457d93b3
simon despres <simondprs62790@hotmail.fr>
2023-09-29 15:21:44 +0200

    2eme box: animation CSS terminé

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,6 +4,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
+  const data = [
+    { word: "Frustration" },
+    { word: "angoisse" },
+    { word: "peur" },
+    { word: "tristesse" },
+    { word: "hyperactivité" },
+    { word: "agrésivité" },
+    { word: "turbulant" },
+    { word: "douleur" },
+    { word: "aboiement" },
+  ];
+
   return (
     <div className="container">
       {/* Navbar  */}
@@ -14,30 +26,55 @@ export default function Home() {
       </section>
       <section>
         {/* 1er box */}
-        <div className="bg-teal-600 rounded-xl relative h-96 -left-10 overflow-hidden">
+        <div className="bg-teal-600 rounded-3xl relative h-96 -left-10 overflow-hidden z-0">
           <div className="circle bg-teal-700 absolute z-0 -left-96 -top-[850px]"></div>
-          <h2 className="flex justify-center py-12 relative z-10">
-            La kinésiologie animalière
-          </h2>
-          <div className="px-12 ml-64 mb-12 ">
-            Retrouver l'harmonie intérieure de votre animal grâce à la
-            libération émotionelle en profondeur.
-          </div>
-          <div className="absolute right-16 z-10">
-            <Link
-              className=" relative px-4 py-2 border hover:border-2 rounded-full text-sm right-0"
-              to="/la_kinésiologie"
-            >
-              En savoir plus{" "}
-              <FontAwesomeIcon className="ml-1" icon={faArrowRight} />
-            </Link>
+          <div className=" w-3/4 border-red-700">
+            <h2 className="flex  py-12 relative z-10">
+              La kinésiologie animalière
+            </h2>
+            <div className="pl-12 ml-48">
+              Retrouver l'harmonie intérieure de votre animal grâce à la
+              libération émotionelle en profondeur.
+            </div>
+            <div className="absolute right-16 bottom-28 z-10">
+              <Link
+                className=" relative px-4 py-2 border hover:border-2 rounded-full text-sm right-0"
+                to="/la_kinésiologie"
+              >
+                En savoir plus{" "}
+                <FontAwesomeIcon className="ml-1" icon={faArrowRight} />
+              </Link>
+            </div>
           </div>
           <div className="circle bg-teal-700 absolute z-0 -right-96 -bottom-[850px]"></div>
         </div>
         {/* 2eme box */}
-        <div className=" bg-gray-800 rounded-xl relative h-96 left-20 overflow-hidden">
-          <h2 className="flex justify-center py-12 relative ">travail sur</h2>
-          <div className="px-12 ml-64 mb-12 ">Animation JS liste probleme</div>
+        <div className=" bg-gray-800 rounded-3xl relative h-60 left-20 -top-24 overflow-hidden z-10">
+          <h2 className=" px-14 pt-6 relative ">travail</h2>
+          <h2 className=" px-14 pb-8 relative ">sur</h2>
+          {/* animation caroussel */}
+          <div className="flex words-slide">
+            {data.map((probleme) => (
+              <span
+                key="probleme.id"
+                className="border-linear-gradient p-[2px] mx-1 rounded-xl"
+              >
+                <div className="word_bg px-7 py-2 rounded-xl">
+                  <span className="word">{probleme.word}</span>
+                </div>
+              </span>
+            ))}
+            {data.map((probleme) => (
+              <span
+                key="probleme.id"
+                className="border-linear-gradient p-[2px] mx-1 rounded-xl"
+              >
+                <div className="word_bg px-7 py-2 rounded-xl">
+                  <span className="word">{probleme.word}</span>
+                </div>
+              </span>
+            ))}
+          </div>
         </div>
       </section>
       <footer className="p-12"></footer>
